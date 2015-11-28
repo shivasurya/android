45c7325e54ce75679b15471c102cdf239cfac55a
s.shivasurya <s.shivasurya@gmail.com>
2015-11-28 08:21:21 +0530

    Added Picasso loaded Image from Network: Popular Movie App

--- app/src/main/java/com/ivb/udacity/ImageAdapter.java
+++ app/src/main/java/com/ivb/udacity/ImageAdapter.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 /**
  * Created by S.Shivasurya on 11/28/2015 - androidStudio.
  */
@@ -15,22 +17,26 @@ public class ImageAdapter extends BaseAdapter {
             R.drawable.rich, R.drawable.rich2, R.drawable.rich3, R.drawable.rich4,
             R.drawable.rich, R.drawable.rich2, R.drawable.rich3, R.drawable.rich4
     };
+    public String[] img = {"http://static.dnaindia.com/sites/default/files/2014/10/27/278392-jeff-bezos.jpg",
+            "https://www.australiansolarquotes.com.au/wp-content/uploads/2015/08/17337481415_cb22284bab_z1.jpg",
+            "http://data1.ibtimes.co.in/cache-img-0-450/en/full/580057/imgmark-zuckerberg.jpg", "http://c7.nrostatic.com/sites/default/files/uploaded/pic_related_082914_SM_Warren-Buffett-G_0.jpg",
+            "http://static.dnaindia.com/sites/default/files/2014/05/28/239989-bg-srinivas-infy.jpg"};
     private LayoutInflater inflater;
+    private Context mContext;
 
     public ImageAdapter(Context c) {
+        mContext = c;
         inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-
     }
 
     @Override
     public int getCount() {
-        return mThumbIds.length;
+        return img.length;
     }
 
     @Override
     public Object getItem(int position) {
-        return mThumbIds[position];
+        return img[position];
     }
 
     @Override
@@ -48,7 +54,10 @@ public class ImageAdapter extends BaseAdapter {
             v.setTag(R.id.grid_image, v.findViewById(R.id.grid_image));
         }
         picture = (ImageView) v.getTag(R.id.grid_image);
-        picture.setImageResource(mThumbIds[position]);
+        Picasso.with(mContext)
+                .load(img[position])
+                .fit()
+                .into(picture);
         return v;
     }
 }
