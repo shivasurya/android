a6bb50dd3554898eb133f3843b3d779d85d1717e
s.shivasurya <s.shivasurya@gmail.com>
2016-01-06 23:23:41 +0530

    Finished UI for Detail Activity - Optimized for Mobile and Tablet need to work on Details API need to work on SQLite fav need to work on share feature and Youtube Intent

--- app/src/main/java/com/ivb/udacity/movieDetailFragment.java
+++ app/src/main/java/com/ivb/udacity/movieDetailFragment.java
@@ -4,16 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.ivb.udacity.adapter.youtubeAdapter;
-import com.ivb.udacity.fragments.youtubeFragment;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A fragment representing a single movie detail screen.
@@ -47,26 +40,9 @@ public class movieDetailFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        if (savedInstanceState == null) {
-            ViewPager pager = (ViewPager) getActivity().findViewById(R.id.youtubefragment);
-            List<Fragment> fList = getYoutubeBanner();
-            youtubeAdapter mYoutubeAdapter = new youtubeAdapter(getActivity().getSupportFragmentManager(), fList);
-            pager.setAdapter(mYoutubeAdapter);
-        }
 
     }
 
-    protected List<Fragment> getYoutubeBanner() {
-        String[] url = new String[]{"http://media1.santabanta.com/full1/Bollywood%20Movies/Guru/gur4e.jpg", "https://madaboutmoviez.files.wordpress.com/2012/04/guru2.jpg", "http://media1.santabanta.com/full1/bollywood%20movies/guru/gur3h.jpg", "http://farm1.static.flickr.com/173/367437103_917159d9f9_o.jpg"};
-        List<Fragment> fragmentList = new ArrayList<>();
-        youtubeFragment[] mYoutubeFragment = new youtubeFragment[4];
-        for (int i = 0; i < 4; i++) {
-            mYoutubeFragment[i] = youtubeFragment.newInstance("hello");
-            mYoutubeFragment[i].LoadImage(url[i]);
-            fragmentList.add(mYoutubeFragment[i]);
-        }
-        return fragmentList;
-    }
 
     @Override
     public void onDestroy() {
